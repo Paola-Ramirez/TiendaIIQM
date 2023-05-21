da8ddb1a36029601fc0b0f40168c5c72afb8f3a0
paolis <mramirez00473@ufide.ac.cr>
2023-05-21 08:59:07 -0600

    Creacion Del Proyecto Inicial

---
+++ src/main/java/com/TiendaIIQM/TiendaIiqmApplication.java
@@ -0,0 +1,13 @@
+package com.TiendaIIQM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaIiqmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaIiqmApplication.class, args);
+	}
+
+}
