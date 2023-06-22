dbfc147fb84999ddc4a3ccbf37efba94749b5a5a
paolis <mramirez00473@ufide.ac.cr>
2023-06-21 18:31:26 -0600

    semana5

--- src/main/java/com/TiendaIIQM/controller/IndexController.java
+++ src/main/java/com/TiendaIIQM/controller/IndexController.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *
  * @author Maria
  */
-@Controller
+@Controller //Esto se llama anotación/
 public class IndexController {
+    //    mapping:URL a los que responde
     @RequestMapping("/url")
     public String page(Model model) {
         model.addAttribute("mensaje", "Hola desde el controlador");
+        //qué vista se tiene que msotrar, NO son als de static, son las de tempaltes
         return "index";
     }
     
